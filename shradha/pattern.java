9ad5d9369ccc08bafff33e004e2f5c198cc7e635
Naveen <nchourasia591@gmail.com>
2025-09-18 22:13:31 +0530

    Pattern

---
+++ shradha/pattern.java
@@ -0,0 +1,37 @@
+package shradha;
+
+public class pattern {
+    public static void main(String[] args) {
+
+
+        /*for (int i = 0; i < 5; i++) {
+
+             for (int j=5; j>i; j--) {
+                // int j = i; j < 5 ; j++
+                 System.out.print("*");
+             }
+            System.out.println();
+    }
+            */
+        //for square
+        /*for (int i = 0; i < 5; i++) {
+
+             for (int j = 0; j <5 ; j++) {
+                 System.out.print("*");
+             }
+            System.out.println();
+    }
+*/
+
+//pattern right angled triangle
+        /*  for (int i = 0; i < 5; i++) {
+
+             for (int j = 1; j <i+1 ; j++) {
+                 System.out.print("*");
+             }
+            System.out.println();
+    }
+            
+        */ 
+    }
+}
