02d7d8e0e4cde631bec2734fbf3428c440c2907c
Naveen <nchourasia591@gmail.com>
2025-09-18 20:59:19 +0530

    update

--- Strivers/index.cpp
+++ Strivers/index.cpp
@@ -1,9 +1,19 @@
-#include<iostream>
-#include<math.h>
-#include<String>
+#include <bits/stdc++.h>
+using namespace std;
 
-int main(){
-    std::cout << "Hey Buddy" <<"\n"; //or std::endl;
+int main() {
+    // int x;
+    // cin >> x;
+    // cout << "Value of x: " << x <<"\n"; // or std::endl;
+
+    cout << "int :"<<numeric_limits<int>::min() << " to "<<numeric_limits<int>::max() <<"\n";
+    cout << "long :"<<numeric_limits<long>::min() << " to "<<numeric_limits<long>::max() <<"\n";
+    cout << "long long :"<<numeric_limits<long long>::min() << " to "<<numeric_limits<long long>::max() <<"\n";
+    cout << "float :"<<numeric_limits<float>::min() << " to "<<numeric_limits<float>::max() <<"\n";
+    cout << "double :"<<numeric_limits<double>::min() << " to "<<numeric_limits<double>::max() <<"\n";
+    cout << "char :"<<numeric_limits<char>::min() << " to "<<numeric_limits<char>::max() <<"\n";
+    cout << "bool :"<<numeric_limits<bool>::min() << " to "<<numeric_limits<bool>::max() <<"\n";
+    
 
     return 0;
 }
