8721f3176dab75e195edab0453288b50cec66580
Naveen <nchourasia591@gmail.com>
2025-09-19 23:30:26 +0530

    Work in progress: pattern changes

--- shradha/pattern.java
+++ shradha/pattern.java
@@ -22,14 +22,14 @@ public class pattern {
 */
 
 //pattern right angled triangle
-        /*  for (int i = 0; i < 5; i++) {
+         for (int i = 0; i < 5; i++) {
 
              for (int j = 1; j <i+1 ; j++) {
-                 System.out.print("*");
+                 System.out.print(j);
              }
             System.out.println();
     }
             
-        */ 
+        
     }
 }
